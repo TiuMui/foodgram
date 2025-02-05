1cd60739773ecac3a4dba1d3d50ac0fd5acf7406
Тимур Мустафин <tiumui@yandex.ru>
2025-02-06 00:51:32 +0300

    Добавлена настройка CSRF_TRUSTED_ORIGINS.

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -10,6 +10,8 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(',')
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
